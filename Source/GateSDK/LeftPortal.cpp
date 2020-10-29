ab88b6e109d756007f70851a5c7add2785b54fa7
Toasty-Laptop <curtisgauntdev@gmail.com>
2020-10-28 21:12:30 -0400

    finalized most constructor elements

--- Source/GateSDK/LeftPortal.cpp
+++ Source/GateSDK/LeftPortal.cpp
@@ -12,19 +12,22 @@ ALeftPortal::ALeftPortal()
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 
 	Plane->SetupAttachment(RootComponent);
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> PlaneAsset(TEXT(""));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> PlaneAsset(TEXT("/Content/StarterContent/Shapes/Shape_Plane.uasset"));
 	if (PlaneAsset.Succeeded())
 	{
 		Plane->SetStaticMesh(PlaneAsset.Object);
+		Plane->SetMobility(EComponentMobility::Movable);
 		Plane->SetRelativeLocation(FVector(0.f, 0.f, 0.f));
-		Plane->SetRelativeRotation();
-		Plane->SetWorldScale3D(FVector(0.f));
+		Plane->SetRelativeRotation(FRotator(90.f, 0.f, -90.f));
+		Plane->SetWorldScale3D(FVector(1.25f, 2.25f, 0.f));
 	}
 
 	SceneCapture->SetupAttachment(RootComponent);
+	SceneCapture->SetRelativeLocation(FVector(10.f, 0.f, 0.f));
 
 	Box->SetupAttachment(RootComponent);
 	Box->SetRelativeLocation(FVector(10.f, 0.f, 0.f));
+	Box->SetWorldScale3D(FVector(0.25f, 1.5f, 3.25f));
 
 	Arrow->SetupAttachment(RootComponent);
 	Arrow->SetRelativeLocation(FVector(10.f, 0.f, 0.f));
